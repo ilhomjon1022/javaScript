0c49130ed8582a57f4a485201df666bd36ecf0b0
ilhomjon1022 <ilhomjonsobirov904@gmail.com>
2023-08-22 14:30:05 +0500

    Dynamic typing

---
+++ dynamic typing/script.js
@@ -0,0 +1,37 @@
+// to string
+// 1-)
+// console.log(typeof String(4))
+
+// // 2-)
+// console.log(typeof ('samar' +21))
+
+// const youtubeChannel = 10
+// console.log('youtube.com/channel/' + youtubeChannel)
+
+
+
+// to number
+// 1-)
+// console.log(typeof Number('22'))
+
+// // 2-)
+// console.log(typeof + '5')
+
+// // 3-)
+// console.log(typeof parseInt('15'))
+
+
+
+
+// to boolean
+// 0, "", undefined, NaN = xar doim false boladi
+
+let age = 0
+
+if (age) {
+    console.log('g')
+}
+
+age = 18
+
+console.log(typeof Boolean('4'))
