cfbab18010bef7c117c36383ff2610fc3f42276a
ilhomjon1022 <ilhomjonsobirov904@gmail.com>
2023-08-29 15:01:35 +0500

    loyiha

---
+++ Real loyiha 29.08/script.js
@@ -0,0 +1,21 @@
+
+
+ const btn = document.querySelector('#btn');
+
+ function myAnimation() {
+   const car = document.querySelector('.car');
+   let pos = 0;
+
+   const timerId = setInterval(frame, 10);
+
+   function frame() {
+     if (pos === 700) {
+       clearInterval(timerId);
+     } else {
+       pos++;
+       car.style.left = pos + 'px';
+     }
+   }
+ }
+
+ btn.addEventListener('click', myAnimation);
